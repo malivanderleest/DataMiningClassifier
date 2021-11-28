459fa94dec6f12f9237ee7b81c639171b0e152c8
Mali Vander Leest <malivanderleest@gmail.com>
2021-11-28 14:58:50 -0600

    progress

--- classifier.py
+++ classifier.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sklearn as sk
 from sklearn.model_selection import StratifiedKFold
-
-
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 
 #-------------------------------------------------------------------------------
 # MAIN
@@ -25,48 +27,136 @@ if __name__ == "__main__":
     # Drop all samples with unknown values
     data = data.loc[data['F'] != '?']
 
+    # Shuffle values
+    training_data = data.sample(frac=1)
+    del training_data['ID']
+
     # Separate benign and malignant tumor data
-    b_data = data.loc[data['J'] == 2]
-    m_data = data.loc[data['J'] == 4]
+    b_data = training_data.loc[training_data['J'] == 2]
+    m_data = training_data.loc[training_data['J'] == 4]
 
     # Allocate 10% to a validation set and maintain ratios
     b_validation = b_data.sample(frac = 0.1)
-    data = data.drop(b_validation.index)
+    training_data = training_data.drop(b_validation.index)
     m_validation = m_data.sample(frac = 0.1)
-    data = data.drop(m_validation.index)
-    validation_data = b_validation.append(m_validation, shuffle = True)
+    training_data = training_data.drop(m_validation.index)
+    validation_data = b_validation.append(m_validation)
 
     # Divide remaining data into 10-fold subsets
-    kfold_class = data['J']
+    kfold_class = training_data['J']
 
-    skf = StratifiedKFold(n_splits=10)
-    for train_index, test_index in skf.split(data, kfold_class):
-        print("\n**************** SUBSET ****************")
-        print("TRAIN:", train_index, "TEST:", test_index)
-        X_train, X_test = data.iloc[train_index], data.iloc[test_index]
-        y_train, y_test = kfold_class.iloc[train_index], kfold_class.iloc[test_index]
+    ################################ HYPERPARAMETERS #################################
+    # k-nearest neighbors
+    k_params = [1,2,4,8,16,32,64,128,256,512]
+    k_params_accuracy = [0,0,0,0,0,0,0,0,0,0]
 
-        # k-nearest neighbors
-        k_param_accuracy = {}
-
-        # Decision tree
-        first_split_param_accuracy = {}
-        
-        # Random Forest
-        first_split_param_accuracy = {}
-
-        # SVM using the polynomial kernel
-        d_param_accuracy = {}
-        
-        # SVM using the RBF kernel
-        gamma_param_accuracy = {}
-        
-        # Deep neural network with sigmoid activation
-        z_param_accuracy = {}
-        
-        # Deep neural network with ReLU activation
-        z_param_accuracy = {}
+    # Decision tree
+    max_depth_params = [1,2,3,4,5,6,7,8,9,10]
+    max_depth_params_accuracy = [0,0,0,0,0,0,0,0,0,0]
     
+    # Random Forest
+    trees_params = [25,50,100,125,150,175,200,225,250,275]
+    trees_params_accuracy = [0,0,0,0,0,0,0,0,0,0]
+
+    # SVM using the polynomial kernel
+    d_params = [1,2,3,4,5,6,7,8,9,10]
+    d_params_accuracy = [0,0,0,0,0,0,0,0,0,0]
+    
+    # SVM using the RBF kernel
+    gamma_params = []
+    gamma_params_accuracy = []
+    
+    # Deep neural network with sigmoid activation
+    z_params = []
+    z_params_accuracy = []
+    
+    # Deep neural network with ReLU activation
+    relu_z_params = []
+    relu_z_params_accuracy = []
+
+    index = 0
+    skf = StratifiedKFold(n_splits=10)
+
+    del training_data['J']
+    print(training_data)
+
+    while index < 10:
+
+        k_total = 0
+        max_depth_total = 0
+        trees_total = 0
+        d_total = 0
+
+    ################################ TRAINING #################################
+        for train_index, test_index in skf.split(training_data, kfold_class):
+            #print("\n**************** SUBSET ****************")
+            #print("TRAIN:", train_index, "TEST:", test_index)
+            X_train, X_test = training_data.iloc[train_index], training_data.iloc[test_index]
+            y_train, y_test = kfold_class.iloc[train_index], kfold_class.iloc[test_index]
+
+            # KNN classifier training using Euclidean distance
+            k_param = k_params[index]
+            knn = KNeighborsClassifier(n_neighbors = k_param)
+            knn.fit(X_train,y_train)
+            #KNN classifier testing
+            k_total = k_total + knn.score(X_test, y_test)
+
+            # Decision tree classifier with different max depths
+            depth = max_depth_params[index]
+            dectree = tree.DecisionTreeClassifier(max_depth = depth)
+            dectree = dectree.fit(X_train, y_train)
+            # Decision tree testing
+            max_depth_total = max_depth_total + dectree.score(X_test, y_test)
+
+            # Random forest classifier with different numbers of trees
+            num_trees = trees_params[index]
+            forest = RandomForestClassifier(n_estimators = num_trees)
+            forest.fit(X_train, y_train)
+            # Random forest testing
+            trees_total = trees_total + forest.score(X_test,y_test)
+
+            # SVM polynomial kernel classifier
+            d = d_params[index]
+            poly_svm = SVC(kernel = 'poly', degree = d)
+            poly_svm.fit(X_train, y_train)
+            # SVM polynomial kernel testing
+            d_total = d_total + poly_svm.score(X_test,y_test)
+
+    ################################ AVG ACCURACY #################################
+        # Average accuracy for k
+        avg_k_accuracy = k_total/10
+        k_params_accuracy[index] = avg_k_accuracy
+
+        # Average accuracy for decision tree depth
+        avg_max_depth_accuracy = max_depth_total/10
+        max_depth_params_accuracy[index] = avg_max_depth_accuracy
+
+        # Average accuracy for forest number of trees
+        avg_trees_accuracy = trees_total/10
+        trees_params_accuracy[index] = avg_trees_accuracy
+
+        # Average accuracy for polynomial kernel degrees
+        avg_d_accuracy = d_total/10
+        d_params_accuracy[index] = avg_d_accuracy
+
+        #print("DEPTH = ", depth)
+        #r = tree.export_text(dectree, feature_names=['A','B','C','D','E','F','G','H','I'])
+        #print(r)
+
+        index = index + 1
+
+    print("************* knn ***************")
+    print(k_params_accuracy)
+
+    print("\n************* decision tree ***************")
+    print(max_depth_params_accuracy)
+
+    print("\n************* random forest ***************")
+    print(trees_params_accuracy)
+
+    print("\n************* svm poly kernel ***************")
+    print(d_params_accuracy)
+
    
     ############################################################################
 
