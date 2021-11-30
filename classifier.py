e81c753de9822c4db14cea50f5e4942f6a98bc17
Mali Vander Leest <malivanderleest@gmail.com>
2021-11-30 14:08:28 -0600

    graphs

--- classifier.py
+++ classifier.py
@@ -11,6 +11,7 @@ from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
+import pprint
 
 #-------------------------------------------------------------------------------
 # MAIN
@@ -49,7 +50,7 @@ if __name__ == "__main__":
 
     ################################ HYPERPARAMETERS #################################
     # k-nearest neighbors
-    k_params = [1,2,4,8,16,32,64,128,256,512]
+    k_params = [1,2,3,4,5,6,7,8,9,10]
     k_params_accuracy = [0,0,0,0,0,0,0,0,0,0]
 
     # Decision tree
@@ -57,8 +58,8 @@ if __name__ == "__main__":
     max_depth_params_accuracy = [0,0,0,0,0,0,0,0,0,0]
     
     # Random Forest
-    #trees_params = [25,50,100,125,150,175,200,225,250,275]
-    trees_params = [1,1,1,1,1,1,1,1,1,1,1]
+    trees_params = [25,50,100,125,150,175,200,225,250,275]
+    #trees_params = [1,1,1,1,1,1,1,1,1,1,1]
     trees_params_accuracy = [0,0,0,0,0,0,0,0,0,0]
 
     # SVM using the polynomial kernel
@@ -70,18 +71,18 @@ if __name__ == "__main__":
     gamma_params_accuracy = [0,0,0,0,0,0,0,0,0,0]
     
     # Deep neural network with sigmoid activation
-    neurons_params = [10,20,30,40,50,60,70,80,90,100]
+    neurons_params = [1,2,3,4,5,6,7,8,9,10]
     neurons_params_accuracy = [0,0,0,0,0,0,0,0,0,0]
     
     # Deep neural network with ReLU activation
-    relu_neurons_params = [10,20,30,40,50,60,70,80,90,100]
+    relu_neurons_params = [1,2,3,4,5,6,7,8,9,10]
     relu_neurons_params_accuracy = [0,0,0,0,0,0,0,0,0,0]
 
     index = 0
     skf = StratifiedKFold(n_splits=10)
 
     del training_data['J']
-    print(training_data)
+    #print(training_data)
 
     while index < 10:
 
@@ -137,10 +138,19 @@ if __name__ == "__main__":
 
             # Sigmoid DNN classifier
             neurons = neurons_params[index]
-            sigmoid_dnn = MLPClassifier(hidden_layer_sizes = neurons, activation = 'logistic', max_iter = 1000)
+            sigmoid_dnn = MLPClassifier(hidden_layer_sizes = neurons, 
+                                        activation = 'logistic', 
+                                        max_iter = 2000)
             sigmoid_dnn.fit(X_train, y_train)
             # Sigmoid DNN testing
             neurons_total = neurons_total + sigmoid_dnn.score(X_test,y_test)
+
+            # ReLu DNN classifier
+            relu_neurons = relu_neurons_params[index]
+            relu_dnn = MLPClassifier(hidden_layer_sizes = neurons, max_iter = 2000)
+            relu_dnn.fit(X_train, y_train)
+            # ReLu DNN testing
+            relu_neurons_total = relu_neurons_total + relu_dnn.score(X_test,y_test)
 
     ################################ AVG ACCURACY #################################
         # Average accuracy for k
@@ -167,6 +177,10 @@ if __name__ == "__main__":
         avg_neurons_accuracy = neurons_total/10
         neurons_params_accuracy[index] = avg_neurons_accuracy
 
+        # Average accuracy for sigmoid DNN number of neurons
+        avg_relu_neurons_accuracy = relu_neurons_total/10
+        relu_neurons_params_accuracy[index] = avg_relu_neurons_accuracy
+
         #print("DEPTH = ", depth)
         #r = tree.export_text(dectree, feature_names=['A','B','C','D','E','F','G','H','I'])
         #print(r)
@@ -174,34 +188,107 @@ if __name__ == "__main__":
         index = index + 1
 
     print("************* knn ***************")
-    print(k_params_accuracy)
+    k_params_map = list(zip(k_params, k_params_accuracy))
+    pprint.pprint(k_params_map)
 
     print("\n************* decision tree ***************")
-    print(max_depth_params_accuracy)
+    max_depth_params_map = list(zip(max_depth_params, max_depth_params_accuracy))
+    pprint.pprint(max_depth_params_map)
 
     print("\n************* random forest ***************")
-    print(trees_params_accuracy)
+    trees_params_map = list(zip(trees_params, trees_params_accuracy))
+    pprint.pprint(trees_params_map)
 
     print("\n************* svm poly kernel ***************")
-    print(d_params_accuracy)
+    d_params_map = list(zip(d_params, d_params_accuracy))
+    pprint.pprint(d_params_map)
 
     print("\n************* svm rbf kernel ***************")
-    print(gamma_params_accuracy)
+    gamma_params_map = list(zip(gamma_params, gamma_params_accuracy))
+    pprint.pprint(gamma_params_map)
 
     print("\n************* dnn sigmoid ***************")
-    print(neurons_params_accuracy)
+    neurons_params_map = list(zip(neurons_params, neurons_params_accuracy))
+    pprint.pprint(neurons_params_map)
+
+    print("\n************* dnn relu ***************")
+    relu_neurons_params_map = list(zip(relu_neurons_params, relu_neurons_params_accuracy))
+    pprint.pprint(relu_neurons_params_map)
 
    
     ############################################################################
 
+    # Hyperparameter accuracy plots
+    if plot:
+        plt.scatter(k_params,k_params_accuracy)
+        plt.plot(list(k_params),list(k_params_accuracy))
+        plt.title('Accuracy of KNN classifier for different k values')
+        plt.xlabel('k (number of nearest neighbors)')
+        plt.ylabel('Accuracy of classifier')
+        plt.show()
+        plt.savefig('knn.png')
+        plt.cla()   # Clear axis
+        plt.clf()   # Clear figure
+        
+        plt.scatter(max_depth_params,max_depth_params_accuracy)
+        plt.plot(list(max_depth_params),list(max_depth_params_accuracy))
+        plt.title('Accuracy of decision tree classifier for different max depth values')
+        plt.xlabel('max depth of tree')
+        plt.ylabel('Accuracy of classifier')
+        plt.show()
+        plt.savefig('dectree.png')
+        plt.cla()   # Clear axis
+        plt.clf()   # Clear figure
 
+        plt.scatter(trees_params, trees_params_accuracy)
+        plt.plot(list(trees_params),list(trees_params_accuracy))
+        plt.title('Accuracy of random forest classifier for different quantities of trees')
+        plt.xlabel('number of trees in forest')
+        plt.ylabel('Accuracy of classifier')
+        plt.show()
+        plt.savefig('randomforest.png')
+        plt.cla()   # Clear axis
+        plt.clf()   # Clear figure
 
-    # Plot univariate regression models
-    
-    # if plot:
-    #     plt.scatter(training['training_x1'], training['training_y'])
-    #     plt.title('Component 1 Trained Univariate Model')
-    #     plt.xlabel('Cement (kg in a m^3 mixture)')
-    #     plt.ylabel('Concrete Compressive Strength (MPa)')
-    #     plt.plot(training['training_x1'], updated_m_params[0]*training['training_x1']+updated_b_params[0], color = "black")
-    #     plt.show()
+        plt.scatter(d_params,d_params_accuracy)
+        plt.plot(list(d_params),list(d_params_accuracy))
+        plt.title('Accuracy of SVM polynomial kernel classifier for different d values')
+        plt.xlabel('d (degree of polynomial kernel)')
+        plt.ylabel('Accuracy of classifier')
+        plt.show()
+        plt.savefig('svmpoly.png')
+        plt.cla()   # Clear axis
+        plt.clf()   # Clear figure
+
+        plt.scatter(gamma_params,gamma_params_accuracy)
+        plt.plot(list(gamma_params),list(gamma_params_accuracy))
+        plt.title('Accuracy of SVM RBF classifier for different gamma values')
+        plt.xlabel('gamma')
+        plt.ylabel('Accuracy of classifier')
+        plt.show()
+        plt.savefig('svmrbf.png')
+        plt.cla()   # Clear axis
+        plt.clf()   # Clear figure
+
+        plt.scatter(neurons_params,neurons_params_accuracy)
+        plt.plot(list(neurons_params),list(neurons_params_accuracy))
+        plt.title('Accuracy of DNN classifier with Sigmoid activation function for different quantities of neurons in hidden layers')
+        plt.xlabel('number of neurons in hidden layers')
+        plt.ylabel('Accuracy of classifier')
+        plt.show()
+        plt.savefig('dnnsigmoid.png')
+        plt.cla()   # Clear axis
+        plt.clf()   # Clear figure
+
+        plt.scatter(relu_neurons_params,relu_neurons_params_accuracy)
+        plt.plot(list(relu_neurons_params),list(relu_neurons_params_accuracy))
+        plt.title('Accuracy of DNN classifier with ReLu activation function for different quantities of neurons in hidden layers')
+        plt.xlabel('number of neurons in hidden layers')
+        plt.ylabel('Accuracy of classifier')
+        plt.show()
+        plt.savefig('dnnrelu.png')
+        plt.cla()   # Clear axis
+        plt.clf()   # Clear figure
+
+        
+
